d518e7826175858c17c52b995e9d1d22ca63d5c7
Ankit Pasayat <ankitpasayat@gmail.com>
2024-02-26 23:28:05 +0530

    Time: 68 ms (15.47%) | Memory: 51.1 MB (11.17%) - LeetSync

--- 1205-defanging-an-ip-address/defanging-an-ip-address.ts
+++ 1205-defanging-an-ip-address/defanging-an-ip-address.ts
@@ -1,3 +1,13 @@
-function defangIPaddr(address: string): string {
-    return address.replaceAll(".", "[.]");
+function defangIPaddr(Ip: string): string {
+    let ans: any = [];
+    Ip.split('');
+    for (let i = 0; i < Ip.length; i++) {
+        if (Ip[i] === ".") {
+            ans.push("[.]");
+        }
+        else {
+            ans.push(Ip[i]);
+        }
+    }
+    return ans.join('');
 };
