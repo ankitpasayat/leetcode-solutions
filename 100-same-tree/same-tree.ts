ea43edefd4c2ee1c7ea1d3c7c4c416eb913be7da
Ankit Pasayat <ankitpasayat@gmail.com>
2024-02-26 12:17:49 +0530

    Time: 59 ms (47.19%) | Memory: 51.9 MB (11.45%) - LeetSync

--- 100-same-tree/same-tree.ts
+++ 100-same-tree/same-tree.ts
@@ -13,9 +13,11 @@
  */
 
 function isSameTree(first: TreeNode | null, second: TreeNode | null): boolean {
-    if (!first && !second) return true;
-    if (!first || !second) return false;
-    return first?.val === second?.val
-        && isSameTree(first?.left, second?.left)
-        && isSameTree(first?.right, second.right);
+    if (!first && !second) {
+        return true;
+    };
+    if (!first || !second) {
+        return false;
+    };
+    return first?.val === second?.val && isSameTree(first?.left, second?.left) && isSameTree(first?.right, second.right);
 }
