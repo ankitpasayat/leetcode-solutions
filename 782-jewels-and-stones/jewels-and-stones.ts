fd2301fb871d8e249c686d7a6aa13855c13d21ba
Ankit Pasayat <ankitpasayat@gmail.com>
2024-02-28 10:09:03 +0530

    Time: 53 ms (83.81%) | Memory: 51.8 MB (34.57%) - LeetSync

--- 782-jewels-and-stones/jewels-and-stones.ts
+++ 782-jewels-and-stones/jewels-and-stones.ts
@@ -1,10 +1,10 @@
 function numJewelsInStones(jewels: string, stones: string): number {
-    let set = new Set([...jewels])
+    let set = new Set([...jewels]);
     let count = 0;
     for (let stone of [...stones]) {
         if (set.has(stone)) {
             count++;
         }
     }
-    return count
+    return count;
 };
