5e7392dc74269f5f3fa4254477ac1c558f1e717d
Ankit Pasayat <ankitpasayat@gmail.com>
2024-03-01 12:49:21 +0530

    Time: 40 ms (45.47%) | Memory: 16.5 MB (96.12%) - LeetSync

--- 3055-maximum-odd-binary-number/maximum-odd-binary-number.py
+++ 3055-maximum-odd-binary-number/maximum-odd-binary-number.py
@@ -1,4 +1,4 @@
 class Solution:
     def maximumOddBinaryNumber(self, s: str) -> str:
-        zero, one = s.count("0"), s.count("1")
-        return "1" * (one - 1) + "0" * (zero) + "1"
+        ones = s.count("1")
+        return ("1" * (ones - 1)) + ("0" * (len(s) - ones)) + "1"
