48c045e1f944ae22022c15a8477e27dbf2248c99
Ankit Pasayat <ankitpasayat@gmail.com>
2024-03-04 09:40:41 +0530

    Time: 63 ms (77.78%) | Memory: 51.8 MB (44.44%) - LeetSync

--- 985-bag-of-tokens/bag-of-tokens.ts
+++ 985-bag-of-tokens/bag-of-tokens.ts
@@ -1,38 +1,29 @@
 function bagOfTokensScore(tokens: number[], power: number): number {
     let score = 0;
-
     if (!tokens.length) {
         return score;
     }
-
     tokens.sort((a, b) => a - b);
-
     if (power < tokens[0]) {
         return score;
     }
-
     let i = 0;
     let j = tokens.length - 1;
-
     while (i <= j) {
         const current = tokens[i];
-
         if (power >= current) {
             power -= current;
-            score++;
-            i++;
+            score += 1;
+            i += 1;
             continue;
         }
-
         if (score && j - i > 1) {
             power += tokens[j];
-            score--;
-            j--;
+            score -= 1;
+            j -= 1;
             continue;
         }
-
         return score;
     }
-
     return score;
 };
