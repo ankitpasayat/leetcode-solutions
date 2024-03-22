26f368318c41b543d4d253c4c5130bffe1ed0899
Ankit Pasayat <ankitpasayat@gmail.com>
2024-03-22 09:31:10 +0530

    Time: 116 ms (91.54%) | Memory: 70 MB (90.38%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.ts
+++ 234-palindrome-linked-list/palindrome-linked-list.ts
@@ -11,15 +11,21 @@
  */
 
 function isPalindrome(head: ListNode | null): boolean {
-    let slow = head, fast = head, prev, temp
-    while (fast && fast.next)
-        slow = slow.next, fast = fast.next.next
-    prev = slow, slow = slow.next, prev.next = null
-    while (slow)
-        temp = slow.next, slow.next = prev, prev = slow, slow = temp
-    fast = head, slow = prev
-    while (slow)
-        if (fast.val !== slow.val) return false
-        else fast = fast.next, slow = slow.next
-    return true
+    let slow = head, fast = head, prev, temp;
+    while (fast && fast.next) {
+        slow = slow.next, fast = fast.next.next;
+    }
+    prev = slow, slow = slow.next, prev.next = null;
+    while (slow) {
+        temp = slow.next, slow.next = prev, prev = slow, slow = temp;
+    }
+    fast = head, slow = prev;
+    while (slow) {
+        if (fast.val !== slow.val) {
+            return false;
+        } else {
+            fast = fast.next, slow = slow.next;
+        }
+    }
+    return true;
 };
