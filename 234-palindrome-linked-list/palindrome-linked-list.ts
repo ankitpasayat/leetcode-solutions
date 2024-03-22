5c9067c244dfbe49706ff42ddf00e95c10868788
Ankit Pasayat <ankitpasayat@gmail.com>
2024-03-22 09:28:18 +0530

    Time: 127 ms (78.85%) | Memory: 69.5 MB (94.81%) - LeetSync

---
+++ 234-palindrome-linked-list/palindrome-linked-list.ts
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function isPalindrome(head: ListNode | null): boolean {
+    let slow = head, fast = head, prev, temp
+    while (fast && fast.next)
+        slow = slow.next, fast = fast.next.next
+    prev = slow, slow = slow.next, prev.next = null
+    while (slow)
+        temp = slow.next, slow.next = prev, prev = slow, slow = temp
+    fast = head, slow = prev
+    while (slow)
+        if (fast.val !== slow.val) return false
+        else fast = fast.next, slow = slow.next
+    return true
+};
