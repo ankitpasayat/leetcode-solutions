16522660537ac8486770c472a13d0848db844fea
Ankit Pasayat <ankitpasayat@gmail.com>
2024-03-25 12:31:22 +0530

    Time: 88 ms (63.44%) | Memory: 58.2 MB (77.42%) - LeetSync

---
+++ 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.ts
@@ -0,0 +1,12 @@
+function findDuplicates(nums: number[]): number[] {
+    const ans = [];
+    const n = nums.length;
+    for (let i = 0; i < n; i++) {
+        const x = Math.abs(nums[i]);
+        if (nums[x - 1] < 0) {
+            ans.push(x);
+        }
+        nums[x - 1] *= -1;
+    }
+    return ans;
+};
