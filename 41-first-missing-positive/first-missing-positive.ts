402113f02ea7149c761fae2ad97f2d2f41f550e8
Ankit Pasayat <ankitpasayat@gmail.com>
2024-03-26 08:36:30 +0530

    Time: 65 ms (90.59%) | Memory: 58.2 MB (72.06%) - LeetSync

---
+++ 41-first-missing-positive/first-missing-positive.ts
@@ -0,0 +1,19 @@
+function firstMissingPositive(nums: number[]): number {
+    const swap = (arr, i, j) => {
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+    };
+
+    const n = nums.length;
+    for (let i = 0; i < n; i++) {
+        while (nums[i] > 0 && nums[i] <= n && nums[i] !== nums[nums[i] - 1]) {
+            swap(nums, i, nums[i] - 1);
+        }
+    }
+
+    for (let i = 0; i < n; i++) {
+        if (nums[i] !== i + 1) {
+            return i + 1;
+        }
+    }
+    return n + 1;
+};
