c0e6f8ed307e26c5f3f1485fcdeadbb6746a7a4a
Ankit Pasayat <ankitpasayat@gmail.com>
2024-03-27 09:24:17 +0530

    Time: 68 ms (76.00%) | Memory: 56.2 MB (33.33%) - LeetSync

---
+++ 713-subarray-product-less-than-k/subarray-product-less-than-k.ts
@@ -0,0 +1,12 @@
+function numSubarrayProductLessThanK(nums: number[], k: number): number {
+    if (k <= 1) return 0;
+    let left = 0, right = 0, product = 1, count = 0;
+    const n = nums.length;
+    while (right < n) {
+        product *= nums[right];
+        while (product >= k) product /= nums[left++];
+        count += 1 + (right - left);
+        right++;
+    }
+    return count;
+};
