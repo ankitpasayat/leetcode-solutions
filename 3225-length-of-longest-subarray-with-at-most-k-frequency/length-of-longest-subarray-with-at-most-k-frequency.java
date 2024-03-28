af09999a6c240dbd25fb6cd46c2d8a50404ececd
Ankit Pasayat <ankitpasayat@gmail.com>
2024-03-28 09:08:56 +0530

    Time: 67 ms (68.11%) | Memory: 60.4 MB (66.23%) - LeetSync

--- 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.java
+++ 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.java
@@ -4,8 +4,9 @@ class Solution {
         int res = 0, i = 0, n = A.length;
         for (int j = 0; j < n; ++j) {
             count.put(A[j], count.getOrDefault(A[j], 0) + 1);
-            while (count.get(A[j]) > k)
+            while (count.get(A[j]) > k) {
                 count.put(A[i], count.get(A[i++]) - 1);
+            }
             res = Math.max(res, j - i + 1);
         }
         return res;
