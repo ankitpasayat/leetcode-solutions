3a2a4c4bb46d9e8859a71981ee11ad7a1c152327
Ankit Pasayat <ankitpasayat@gmail.com>
2024-03-30 21:53:36 +0530

    Time: 4 ms (99.30%) | Memory: 46.4 MB (77.07%) - LeetSync

--- 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.java
+++ 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.java
@@ -3,14 +3,16 @@ class Solution {
         int res = 0, sz = nums.length;
         int[] cnt = new int[sz + 1];
         for (int l = 0, m = 0, r = 0; r < sz; ++r) {
-            if (++cnt[nums[r]] == 1)
+            if (++cnt[nums[r]] == 1) {
                 if (--k < 0) {
                     cnt[nums[m++]] = 0;
                     l = m;
                 }
+            }
             if (k <= 0) {
-                while (cnt[nums[m]] > 1)
+                while (cnt[nums[m]] > 1) {
                     --cnt[nums[m++]];
+                }
                 res += m - l + 1;
             }
         }
