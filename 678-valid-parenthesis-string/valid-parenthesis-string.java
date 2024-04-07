6ead025bd100dc0adb6a0366be2b04723e0a3a91
Ankit Pasayat <ankitpasayat@gmail.com>
2024-04-07 19:09:15 +0530

    Time: 0 ms (100.00%) | Memory: 41.1 MB (73.12%) - LeetSync

---
+++ 678-valid-parenthesis-string/valid-parenthesis-string.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean checkValidString(String s) {
+        int low = 0;
+        int high = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                low += 1;
+                high += 1;
+            } else if (s.charAt(i) == ')') {
+                if (low > 0) {
+                    low -= 1;
+                }
+                high -= 1;
+            } else {
+                if (low > 0) {
+                    low -= 1;
+                }
+                high += 1;
+            }
+            if (high < 0) {
+                return false;
+            }
+        }
+        return low == 0;
+    }
+}
