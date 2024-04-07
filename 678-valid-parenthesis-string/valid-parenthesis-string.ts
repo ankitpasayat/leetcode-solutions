1bc5c79d99bb0943fb7dfd64671dbf7c5b22cc2f
Ankit Pasayat <ankitpasayat@gmail.com>
2024-04-07 19:10:56 +0530

    Time: 62 ms (46.03%) | Memory: 51.4 MB (44.44%) - LeetSync

---
+++ 678-valid-parenthesis-string/valid-parenthesis-string.ts
@@ -0,0 +1,22 @@
+function checkValidString(s: string): boolean {
+    let leftMin = 0, leftMax = 0;
+    for (let c of s) {
+        if (c === '(') {
+            leftMin += 1;
+            leftMax += 1;
+        } else if (c === ')') {
+            leftMin -= 1;
+            leftMax -= 1;
+        } else {
+            leftMin -= 1;
+            leftMax += 1;
+        }
+        if (leftMax < 0) {
+            return false;
+        }
+        if (leftMin < 0) {
+            leftMin = 0;
+        }
+    }
+    return leftMin === 0;
+};
